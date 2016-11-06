9300032e1180f5bab21b545ff87a93098bffa59f
Nathan Yiangsupapaanontr <zartre.y@gmail.com>
2016-11-06 16:28:44 +0700

    Move Upload action into ArtController

---
+++ app/Http/Controllers/ArtController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class ArtController extends Controller
+{
+    public function upload(Request $request)
+    {
+        $artFile = $request->file('art'); # gets the uploaded file
+        $artFileExtension = strtolower($artFile->extension()); # gets the file's extension
+        $acceptedFileTypes = ["png", "jpg", "jpeg"];
+        if (in_array($artFileExtension, $acceptedFileTypes)) {
+            $cat = $request->input('category'); # gets the category
+            $artName = $request->input('art-name'); # gets the title
+            $time = new \DateTime(); # gets a date-time to use timestamp
+            $fileName = \Auth::user()->id . $time->getTimestamp() . "." . $artFileExtension; # renames the file
+            if ($cat == "abstract-art") {
+                $artFile->storeAs('arts/' . $cat, $fileName);
+            } elseif ($cat == "drawings") {
+                $artFile->storeAs('arts/' . $cat, $fileName);
+            } elseif ($cat == "fan-art") {
+                $artFile->storeAs('arts/' . $cat, $fileName);
+            } elseif ($cat == "paintings") {
+                $artFile->storeAs('arts/' . $cat, $fileName);
+            } elseif ($cat == "sketches") {
+                $artFile->storeAs('arts/' . $cat, $fileName);
+            }
+            $uploadNotice = '<div class="callout success">Successfully uploaded your photo</div>';
+            return view('layout.upload', compact('uploadNotice'));
+        } else {
+            $uploadNotice = '<div class="callout alert">The uploaded photo was not a PNG or JPG file.</div>';
+            return view('layout.upload', compact('uploadNotice'));
+        }
+    }
+}
