be9aee244a6799fa22b9bc4199afee2493b4aa9c
Nathan Yiangsupapaanontr <zartre.y@gmail.com>
2016-11-07 21:18:52 +0700

    Delete unnecessary migrations

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,9 +15,9 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
+            $table->string('name')->unique();
             $table->string('email')->unique();
-            $table->integer('endorse')->nullable();
+            $table->integer('endorse')->unsigned()->default(0);
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
