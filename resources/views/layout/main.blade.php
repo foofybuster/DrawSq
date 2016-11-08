0e88bb833784f26bb661e9e98993b59dde6bac5b
Nathan Yiangsupapaanontr <zartre.y@gmail.com>
2016-11-08 23:04:42 +0700

    Add favicon

--- resources/views/layout/main.blade.php
+++ resources/views/layout/main.blade.php
@@ -4,6 +4,7 @@
     <title>{{ $title or "Drawsquare" }}</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="theme-color" content="#f25a81">
+    <link rel="shortcut icon" type="image/png" href="{{ asset('favicon.png') }}"/>
     <link rel="stylesheet" href="https://webfonts.fontstand.com/WF-004772-06c214436396e98a7ca60aed2c2b7cfb.css"
           type="text/css"/>
     <link href="https://fonts.googleapis.com/css?family=Roboto:400,700" rel="stylesheet">
