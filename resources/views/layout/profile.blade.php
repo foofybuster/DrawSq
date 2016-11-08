bdc1a5d8609ffaf5338ff91ac4a3a45692a38b7a
Nathan Yiangsupapaanontr <zartre.y@gmail.com>
2016-11-08 22:51:13 +0700

    Add no art placeholder to user profile

--- resources/views/layout/profile.blade.php
+++ resources/views/layout/profile.blade.php
@@ -8,14 +8,19 @@
     </section>
     <section id="timeline">
         <div class="row small-up-2 medium-up-3 large-up-4 tile-group" id="timeline-profile">
-            @foreach ($arts->reverse() as $art)
-                <div class="column thumb">
-                    <a href="{{ url('art/' . $art->art_id) }}">
-                        <div class="art-thumb"
-                             style="background-image: url('{{ Storage::url('arts/' . $art->art_cat . "/" . $art->art_file) }}');"></div>
-                    </a>
-                </div>
-            @endforeach
+            @if (count($arts) > 0)
+                @foreach ($arts->reverse() as $art)
+                    <div class="column thumb">
+                        <a href="{{ url('art/' . $art->art_id) }}">
+                            <div class="art-thumb"
+                                 style="background-image: url('{{ Storage::url('arts/' . $art->art_cat . "/" . $art->art_file) }}');">
+                            </div>
+                        </a>
+                    </div>
+                @endforeach
+            @else
+                <h2 class="no-upload">{{ $nameOfUser }} hasn't uploaded any art</h2>
+            @endif
         </div>
         <div class="row">
             <div class="small-12 columns">
