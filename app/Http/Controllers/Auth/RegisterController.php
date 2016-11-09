74c06ff3dca2776cb3f40ce1c38c01ad86a2e7f8
Nathan Yiangsupapaanontr <zartre.y@gmail.com>
2016-11-09 18:52:52 +0700

    Require user to accept ToS

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -51,6 +51,7 @@ class RegisterController extends Controller
             'name' => 'required|max:255|unique:users',
             'email' => 'required|email|max:255|unique:users',
             'password' => 'required|min:6|confirmed',
+            'condition' => 'required',
         ]);
     }
 
--- resources/views/layout/login.blade.php
+++ resources/views/layout/login.blade.php
@@ -49,7 +49,7 @@
                         <label>Password Confirm</label>
                         <input id="password-confirm" type="password" name="password_confirmation" required
                                placeholder="Password Confirm">
-                        <input id="condition" type="checkbox" required>
+                        <input id="condition" type="checkbox" name="condition" required>
                         <label for="condition">I accept <a>terms and conditions</a></label>
                         <button type="submit" class="button expanded rounded">REGISTER NOW!</button>
                     </div>
